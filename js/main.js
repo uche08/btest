6b740ff3d73419f0aa37dd0121f10220402fe0f1
Uche <madiebouche@gmail.com>
2023-02-15 04:51:06 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -92,7 +92,22 @@
         $(this).addClass('active');
 
         portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
+    }); 
     
 })(jQuery);
 
+//Read more function
+function showMoreFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
